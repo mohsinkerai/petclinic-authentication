21b518635d637f5818a6693d5953d906c6bb138c
Mohsin Mansoor Kerai <mohsin.kerai@careem.com>
2017-10-30 19:58:13 +0500

    Working Upload

--- src/main/java/com/system/demo/volunteer/VolunteerController.java
+++ src/main/java/com/system/demo/volunteer/VolunteerController.java
@@ -12,6 +12,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @Controller
@@ -95,5 +97,16 @@ public class VolunteerController {
         } else {
             throw new RuntimeException("Invalid User ID" + id);
         }
+    }
+
+    @RequestMapping(value = "/file")
+    public String file() {
+        return "volunteer/file";
+    }
+
+    @RequestMapping(value = "/file", method = RequestMethod.POST)
+    public String fileSave(@RequestParam("file") MultipartFile file) {
+        log.info("Received file {}", file);
+        return "redirect:/volunteer";
     }
 }
