2983f00bfaba533b999f33c965442ddef6ece091
Zeeshan Dämani <zeeshan.damani@careem.com>
2017-11-22 21:18:02 +0500

    Removed Vehicle and update user code for North. Added excel fields updated.

--- src/main/java/com/system/demo/bulk/BulkJobBuilder.java
+++ src/main/java/com/system/demo/bulk/BulkJobBuilder.java
@@ -1,13 +1,11 @@
 package com.system.demo.bulk;
 
-import com.system.demo.bulk.vehicle.VehicleItemWriterListener;
 import com.system.demo.bulk.volunteer.job.elements.VolunteerBulkProcessor;
 import com.system.demo.bulk.volunteer.job.elements.listener.VolunteerItemReaderListener;
 import com.system.demo.bulk.volunteer.job.elements.listener.VolunteerItemWriterListener;
-import com.system.demo.bulk.volunteer.volunteerupadate.VolunteerUpdateProcessor;
+import com.system.demo.bulk.volunteer.job.elements.listener.VolunteerJobNotificationListener;
 import com.system.demo.vehicle.Vehicle;
 import com.system.demo.volunteer.Volunteer;
-import com.system.demo.bulk.volunteer.job.elements.listener.VolunteerJobNotificationListener;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -19,8 +17,6 @@ import org.springframework.batch.item.database.JpaItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.Resource;
 
 /** Created by Zeeshan Damani */
 @Slf4j
@@ -37,12 +33,10 @@ public class BulkJobBuilder {
   private final JobBuilderFactory jobBuilderFactory;
   private final JpaItemWriter<Volunteer> volunteerBulkWriter;
   private final VolunteerItemWriterListener itemLoggerListener;
-  private final VehicleItemWriterListener vehicleItemWriterListener;
   private final VolunteerBulkProcessor volunteerBulkProcessor;
   private final VolunteerJobNotificationListener volunteerJobNotificationListener;
   private final FlatFileItemReader<Vehicle> vehicleFlatFileItemReader;
   private final JpaItemWriter<Vehicle> vehicleJpaItemWriter;
-  private final VolunteerUpdateProcessor volunteerUpdateProcessor;
   private final VolunteerItemReaderListener volunteerItemReaderListener;
 
   public BulkJobBuilder(
@@ -54,12 +48,10 @@ public class BulkJobBuilder {
       JobBuilderFactory jobBuilderFactory,
       JpaItemWriter<Volunteer> volunteerBulkWriter,
       VolunteerItemWriterListener itemLoggerListener,
-      VehicleItemWriterListener vehicleItemWriterListener,
       VolunteerBulkProcessor volunteerBulkProcessor,
       VolunteerJobNotificationListener volunteerJobNotificationListener,
       FlatFileItemReader<Vehicle> vehicleFlatFileItemReader,
       JpaItemWriter<Vehicle> vehicleJpaItemWriter,
-      VolunteerUpdateProcessor volunteerUpdateProcessor,
       VolunteerItemReaderListener volunteerItemReaderListener) {
     this.volunteerUpdateReader = volunteerUpdateReader;
     this.volunteerItemReader = volunteerItemReader;
@@ -67,12 +59,10 @@ public class BulkJobBuilder {
     this.jobBuilderFactory = jobBuilderFactory;
     this.volunteerBulkWriter = volunteerBulkWriter;
     this.itemLoggerListener = itemLoggerListener;
-    this.vehicleItemWriterListener = vehicleItemWriterListener;
     this.volunteerBulkProcessor = volunteerBulkProcessor;
     this.volunteerJobNotificationListener = volunteerJobNotificationListener;
     this.vehicleFlatFileItemReader = vehicleFlatFileItemReader;
     this.vehicleJpaItemWriter = vehicleJpaItemWriter;
-    this.volunteerUpdateProcessor = volunteerUpdateProcessor;
     this.volunteerItemReaderListener = volunteerItemReaderListener;
 
   }
@@ -90,50 +80,6 @@ public class BulkJobBuilder {
             .listener(volunteerItemReaderListener)
             .faultTolerant()
             .skipPolicy(new DatabaseJobSkipPolicy()).skipLimit(0)
-            .build();
-
-    return jobBuilderFactory
-        .get(jobName)
-        .incrementer(new RunIdIncrementer())
-        .listener(volunteerJobNotificationListener)
-        .flow(step)
-        .end()
-        .build();
-  }
-
-  public Job buildVehicleUpload(String jobName) {
-
-    Step step =
-        stepBuilderFactory
-            .get(UploadTypes.VehicleUpload.toString())
-            .<Vehicle, Vehicle>chunk(2)
-            .reader(vehicleFlatFileItemReader)
-            .writer(vehicleJpaItemWriter)
-            .listener(vehicleItemWriterListener)
-            .faultTolerant()
-            .skipPolicy(new DatabaseJobSkipPolicy())
-            .build();
-
-    return jobBuilderFactory
-        .get(jobName)
-        .incrementer(new RunIdIncrementer())
-        .listener(volunteerJobNotificationListener)
-        .flow(step)
-        .end()
-        .build();
-  }
-
-  public Job updateVolunteer(String jobName) {
-
-    Step step =
-        stepBuilderFactory
-            .get(UploadTypes.VehicleUpload.toString())
-            .<Volunteer, Volunteer>chunk(2)
-            .reader(volunteerUpdateReader)
-            .processor(volunteerUpdateProcessor)
-            .listener(vehicleItemWriterListener)
-            .faultTolerant()
-            .skipPolicy(new DatabaseJobSkipPolicy())
             .build();
 
     return jobBuilderFactory
--- src/main/java/com/system/demo/system/config/BatchConfiguration.java
+++ src/main/java/com/system/demo/system/config/BatchConfiguration.java
@@ -1,8 +1,6 @@
 package com.system.demo.system.config;
 
 import com.system.demo.bulk.volunteer.job.elements.VolunteerFieldSetMapper;
-import com.system.demo.bulk.volunteer.volunteerupadate.VolunteerUpdateFieldSetMapper;
-import com.system.demo.vehicle.Vehicle;
 import com.system.demo.volunteer.Volunteer;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.StepScope;
@@ -14,11 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import com.system.demo.bulk.vehicle.VehicleFieldSetMapper;
 
 import javax.sql.DataSource;
 
@@ -58,30 +54,6 @@ public class BatchConfiguration {
         return reader;
     }
 
-    @Bean(name = "vehicleItemReader")
-    @StepScope
-    public FlatFileItemReader<Vehicle> vehicleCsvReader(@Value("#{jobParameters['sourceFilePath']}") String path) throws Exception {
-
-        FlatFileItemReader<Vehicle> reader = new FlatFileItemReader<Vehicle>();
-        DelimitedLineTokenizer delimitedLineTokenizer = new DelimitedLineTokenizer();
-        Resource resource = resourceLoader.getResource("file:" + path);
-
-        DefaultLineMapper<Vehicle> defaultLineMapper = new DefaultLineMapper<>();
-        defaultLineMapper.setLineTokenizer(delimitedLineTokenizer);
-
-        defaultLineMapper.setFieldSetMapper(new VehicleFieldSetMapper());
-        reader.setLineMapper(defaultLineMapper);
-
-        reader.setLinesToSkip(1);
-        reader.setResource(resource);
-        reader.setSkippedLinesCallback(headerLine -> {
-            delimitedLineTokenizer.setNames(headerLine.split(","));
-        });
-
-        return reader;
-    }
-
-
     @Bean(name = "volunteerItemWriter")
     @StepScope
     public JpaItemWriter<Volunteer> voulunteerItemWriter() throws Exception {
@@ -90,38 +62,5 @@ public class BatchConfiguration {
         writer.setEntityManagerFactory(entityManagerFactory.getObject());
 
         return writer;
-    }
-
-
-    @Bean(name = "vehicleItemWriter")
-    @StepScope
-    public JpaItemWriter<Vehicle> vehicleJpaItemWriter() throws Exception {
-
-        JpaItemWriter writer = new JpaItemWriter<Vehicle>();
-        writer.setEntityManagerFactory(entityManagerFactory.getObject());
-
-        return writer;
-    }
-
-
-    @Bean(name = "volunteerUpdateItemReader")
-    @StepScope
-    public FlatFileItemReader<Volunteer> volunteerUpdate(@Value("#{jobParameters['sourceFilePath']}") String path) throws Exception {
-
-        FlatFileItemReader<Volunteer> reader = new FlatFileItemReader<Volunteer>();
-        DelimitedLineTokenizer delimitedLineTokenizer = new DelimitedLineTokenizer();
-        Resource resource = resourceLoader.getResource("file:" + path);
-        DefaultLineMapper<Volunteer> defaultLineMapper = new DefaultLineMapper<>();
-        defaultLineMapper.setLineTokenizer(delimitedLineTokenizer);
-        defaultLineMapper.setFieldSetMapper(new VolunteerUpdateFieldSetMapper());
-        reader.setLineMapper(defaultLineMapper);
-
-        reader.setLinesToSkip(1);
-        reader.setResource(resource);
-        reader.setSkippedLinesCallback(headerLine -> {
-            delimitedLineTokenizer.setNames(headerLine.split(","));
-        });
-
-        return reader;
     }
 }
